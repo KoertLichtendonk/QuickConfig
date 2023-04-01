85d36eb09880c95c06e6a29e6a7ecb830d57e5ae
Koert Lichtendonk <git@koertlichtendonk.nl>
2023-04-01 21:59:21 +0200

    Add project files.

---
+++ QuickConfig/ConfigManager.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System.Buffers;
+using System.IO;
+
+namespace QuickConfig
+{
+    public class ConfigManager
+    {
+        private string projectName { get; set; }
+        private Dictionary<string, object> configs { get; set; }
+
+        public ConfigManager(string projectName)
+        {
+            this.projectName = projectName;
+            this.configs = new Dictionary<string, object>();
+        }
+
+        public T GetConfig<T>(string configName) where T : IConfig, new()
+        {
+            if(configs.TryGetValue(configName, out object savedConfig))
+            {
+                if(savedConfig is T)
+                {
+                    return (T)savedConfig;
+                }
+            }
+
+            string dirPath = String.Format("{0}{1}{2}", Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), Path.DirectorySeparatorChar, projectName);
+
+            if (!(Directory.Exists(dirPath)))
+            {
+                Directory.CreateDirectory(dirPath);
+            }
+
+            string configPath = String.Format("{0}{1}{2}.json", dirPath, Path.DirectorySeparatorChar, configName);
+
+            T configData;
+            if (!(File.Exists(configPath)))
+            {
+                using (StreamWriter sw = File.CreateText(configPath))
+                {
+                    configData = new T();
+
+                    string configJson = JsonConvert.SerializeObject(configData, Formatting.Indented);
+
+                    sw.Write(configJson);
+                    sw.Close();
+                }
+            }
+
+            string config = File.ReadAllText(configPath);
+
+            configData = JsonConvert.DeserializeObject<T>(config);
+            configData.configPath = configPath;
+
+            configs.Add(configName, configData);
+
+            return configData;
+        }
+    }
+}
---
+++ QuickConfig/IConfig.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickConfig
+{
+    public interface IConfig
+    {
+        public string configPath { get; set; }
+
+        void Save()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+            using (StreamWriter sw = new StreamWriter(configPath, false))
+            {
+                sw.Write(json);
+                sw.Close();
+            }
+        }
+    }
+}
