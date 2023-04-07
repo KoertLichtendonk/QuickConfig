228f9a6f2e83ae6502f981e458206586710beedb
Koert Lichtendonk <C.Lichtendonk@live.nl>
2023-04-08 01:40:47 +0200

    upd

---
+++ QuickConfig/ConfigBase.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickConfig
+{
+    public abstract class ConfigBase : IConfig
+    {
+        public string configPath { get; set; }
+
+        public virtual void Save()
+        {
+            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
+
+            using (StreamWriter sw = new StreamWriter(configPath, false))
+            {
+                sw.Write(json);
+                sw.Close();
+            }
+        }
+    }
+}
--- QuickConfig/IConfig.cs
+++ QuickConfig/IConfig.cs
@@ -11,15 +11,6 @@ namespace QuickConfig
     {
         public string configPath { get; set; }
 
-        void SaveDefault()
-        {
-            string json = JsonConvert.SerializeObject(this, Formatting.Indented);
-
-            using (StreamWriter sw = new StreamWriter(configPath, false))
-            {
-                sw.Write(json);
-                sw.Close();
-            }
-        }
+        void Save();
     }
 }
